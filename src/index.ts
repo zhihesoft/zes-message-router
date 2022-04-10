6d79b563b154ed58460ed943789a983f770fb32f
Stanley <houyuqing@hotmail.com>
2022-04-10 11:45:52 +0800

    update version to 1.0.5

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,8 @@
-import { Router } from "express";
 import "reflect-metadata";
-import { buildExpressRoute, setGuard } from "./lib/build-express-router";
-import { MessageRouter } from "./lib/message-router";
-import { SecurityGuard } from "./lib/security-guard";
 
+export { HttpMessage } from "./lib/decorators";
+export { ExpressHost } from "./lib/engine-host-express";
+export { MessageEngine } from "./lib/message-engine";
 export { MessageProcessor, MessageRouter } from "./lib/message-router";
 export { SecurityGuard } from "./lib/security-guard";
-
-export function forExpress(router: MessageRouter, guard: SecurityGuard): Router {
-    setGuard(guard);
-    const ret = Router();
-    buildExpressRoute(ret, router);
-    return ret;
-}
 
